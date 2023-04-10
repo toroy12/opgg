e505b4c377dd947f5aa81802618353b1fdc0acd5
toroy <toroy@DESKTOP-32BBT50>
2023-04-11 04:50:24 +0900

    [230410 김대현] 롤 전적 검색사이트 - OPGG 사이트 카피 -게시판 추가 	게시판 리스트 	게시판 상세보기 	게시판 작성 	게시판 수정 	게시판 삭제 	게시판 검색 	게시판 페이징 	게시판 작성시 줄바꿈 추가 -디자인 및 오류해결 	전적검색시 솔로랭크전적 또는 자유랭크전적이 없어서 오류가 뜨는 문제 해결 	솔로랭크전적 또는 자유랭크 전적이 없을 시 언랭 마크와 언랭 표시 추가 	나브바 푸터 디자인 변경 	나브바에 커뮤니티 링크와 소환사명 검색기능 추가
    -추후 개선점
    	전적검색기능 활성화(승패및 사용 챔피언, 아이템 등) <-어려워보임
    	로그인 기능추가
    	로그인 기능제작시 oauth 기능을 사용해보고자함
    	비밀번호 찾기시 api를 이용해 메일을 보내는 기능 또한 사용해 보고자함

--- opgg/src/main/java/gg/op/controller/SearchSummoner.java
+++ opgg/src/main/java/gg/op/controller/SearchSummoner.java
@@ -20,58 +20,104 @@ import gg.op.dto.Summoner;
 
 @org.springframework.stereotype.Controller
 public class SearchSummoner {
-	public static String API_KEY = "RGAPI-57050e89-3c74-4326-a627-db119f915d31";
+	public static String API_KEY = "RGAPI-16d0cfa8-d28e-409f-977b-016ecbcbe478";
 
 	@RequestMapping("/search")
 	public String search(Model model, String summonerName) {
-		
+
 		String returnCheck = null;
-		
+
 		Summoner temp = summoner(summonerName);
-		
-		    if(temp != null) {
-		    	LeagueEntryDTO temp2 = soloQueue(temp.getId());
-		    	LeagueEntryDTO temp3 = freeQueue(temp.getId());
-		    	List<String> temp4 = matchList(temp.getPuuid());
-				/* ParticipantDto temp5 = matchGame(temp4.get(0)); */
-		    	
-		    	model.addAttribute("title", summonerName + " - 게임 전적 - League of Legends");
-		    	model.addAttribute("imgURL", "http://ddragon.leagueoflegends.com/cdn/9.16.1/img/profileicon/"+temp.getProfileIconId()+".png");
-		    	model.addAttribute("tierImgURL1", "img/emblems/emblem-" + temp2.getTier() + ".png");
-		    	model.addAttribute("tierImgURL2", "img/emblems/emblem-" + temp3.getTier() + ".png");
-		    	model.addAttribute("temp", temp);
-		    	model.addAttribute("temp2", temp2);
-		    	model.addAttribute("temp3", temp3);
-		    	model.addAttribute("temp4", temp4);
-		    	
-		    	returnCheck = "user/result";
-		    }else {
-		    	
-		    	model.addAttribute("title", "롤 전적 검색 OP.GG - 전적 검색");
-		    	
-		    	returnCheck = "user/nonExistentName";
-		    }
-			
+
+		// 소환사명이 존재할때
+		if (temp != null) {
+			LeagueEntryDTO temp2 = soloQueue(temp.getId());
+			//솔랭전적이 존재할때
+			if(temp2 != null) {
+				LeagueEntryDTO temp3 = freeQueue(temp.getId());
+				//솔랭전적이 있으면서 자유랭전적이 있을때
+				if(temp3 != null) {
+					
+					//List<String> temp4 = matchList(temp.getPuuid());
+					
+					model.addAttribute("title", summonerName + " - 게임 전적 - League of Legends");
+					model.addAttribute("imgURL", "http://ddragon.leagueoflegends.com/cdn/9.16.1/img/profileicon/"
+							+ temp.getProfileIconId() + ".png");
+					model.addAttribute("tierImgURL1", "img/emblems/emblem-" + temp2.getTier() + ".png");
+					model.addAttribute("tierImgURL2", "img/emblems/emblem-" + temp3.getTier() + ".png");
+					model.addAttribute("temp", temp);
+					model.addAttribute("temp2", temp2);
+					model.addAttribute("temp3", temp3);
+					//model.addAttribute("temp4", temp4);
+					
+					returnCheck = "user/result";
+					// 솔랭전적이 있는데 자유랭 전적이 없을때
+				} else {
+					model.addAttribute("title", summonerName + " - 게임 전적 - League of Legends");
+					model.addAttribute("imgURL", "http://ddragon.leagueoflegends.com/cdn/9.16.1/img/profileicon/"
+							+ temp.getProfileIconId() + ".png");
+					model.addAttribute("tierImgURL1", "img/emblems/emblem-" + temp2.getTier() + ".png");
+					model.addAttribute("tierImgURL2", "img/emblems/emblem-unlanked.png");
+					model.addAttribute("temp", temp);
+					model.addAttribute("temp2", temp2);
+					
+					returnCheck = "user/result";
+				}
+				//솔랭전적이 없을때
+			} else {
+				LeagueEntryDTO temp3 = freeQueue(temp.getId());
+				// 솔랭전적이 없는데 자유랭전적이 있을때
+				if(temp3 != null) {
+					model.addAttribute("title", summonerName + " - 게임 전적 - League of Legends");
+					model.addAttribute("imgURL", "http://ddragon.leagueoflegends.com/cdn/9.16.1/img/profileicon/"
+							+ temp.getProfileIconId() + ".png");
+					model.addAttribute("tierImgURL1", "img/emblems/emblem-unlanked.png");
+					model.addAttribute("tierImgURL2", "img/emblems/emblem-" + temp3.getTier() + ".png");
+					model.addAttribute("temp", temp);
+					model.addAttribute("temp3", temp3);
+					
+					returnCheck = "user/result";
+					// 솔랭적적이 없는데 자유랭전적도 없을때
+				} else {
+					model.addAttribute("title", summonerName + " - 게임 전적 - League of Legends");
+					model.addAttribute("imgURL", "http://ddragon.leagueoflegends.com/cdn/9.16.1/img/profileicon/"
+							+ temp.getProfileIconId() + ".png");
+					model.addAttribute("tierImgURL1", "img/emblems/emblem-unlanked.png");
+					model.addAttribute("tierImgURL2", "img/emblems/emblem-unlanked.png");
+					model.addAttribute("temp", temp);
+					
+					returnCheck = "user/result";
+				}
+			}
+			// 소환사명이 존재하지 않을때
+		} else {
+
+			model.addAttribute("title", "롤 전적 검색 OP.GG - 전적 검색");
+
+			returnCheck = "user/nonExistentName";
+		}
+
 		return returnCheck;
 	}
-	
+
 	public Summoner summoner(String summonerName) {
-		
+
 		BufferedReader br = null;
 		Summoner summoner = null;
-		
-		try{            
+
+		try {
 			String SummonerName = summonerName.replaceAll(" ", "%20");
-			String urlstr = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + SummonerName + "?api_key=" + API_KEY;
+			String urlstr = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + SummonerName
+					+ "?api_key=" + API_KEY;
 			URL url = new URL(urlstr);
 			HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
 			urlconnection.setRequestMethod("GET");
-			br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8")); 
+			br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(), "UTF-8"));
 			String result = "";
 			String line;
-			while((line = br.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 				result = result + line;
-			}	
+			}
 			JsonParser jsonParser = new JsonParser();
 			JsonObject k = (JsonObject) jsonParser.parse(result);
 			int profileIconId = k.get("profileIconId").getAsInt();
@@ -81,160 +127,126 @@ public class SearchSummoner {
 			long revisionDate = k.get("revisionDate").getAsLong();
 			String id = k.get("id").getAsString();
 			String accountId = k.get("accountId").getAsString();
-			
+
 			summoner = new Summoner(profileIconId, name, puuid, summonerLevel, revisionDate, id, accountId);
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		return summoner;
 	}
-		
-		
-	
+
 	public LeagueEntryDTO soloQueue(String id) {
-		
+
 		BufferedReader br = null;
 		LeagueEntryDTO leagueInfo = null;
-		
-		try{            
-			String urlstr = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/"+ id + "?api_key=" + API_KEY;
+
+		try {
+			String urlstr = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + id + "?api_key="
+					+ API_KEY;
 			URL url = new URL(urlstr);
 			HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
 			urlconnection.setRequestMethod("GET");
-			br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8")); 
+			br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(), "UTF-8"));
 			String result = "";
 			String line;
-			while((line = br.readLine()) != null) { 
+			while ((line = br.readLine()) != null) {
 				result = result + line;
 			}
 			JsonParser jsonParser = new JsonParser();
 			JsonArray arr = (JsonArray) jsonParser.parse(result);
-			JsonObject k =  (JsonObject) arr.get(0);
+			JsonObject k = (JsonObject) arr.get(0);
 			int wins = k.get("wins").getAsInt();
 			int losses = k.get("losses").getAsInt();
 			String rank = k.get("rank").getAsString();
 			String tier = k.get("tier").getAsString();
 			int leaguePoints = k.get("leaguePoints").getAsInt();
-			
-			int odds = Math.round(((float) wins / (wins + losses))*100);
-			
-			leagueInfo = new LeagueEntryDTO(wins, losses, rank,tier, leaguePoints, odds);
-			
-		}catch(Exception e){
+
+			int odds = Math.round(((float) wins / (wins + losses)) * 100);
+
+			leagueInfo = new LeagueEntryDTO(wins, losses, rank, tier, leaguePoints, odds);
+
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		return leagueInfo;
 	}
-	
-	
+
 	public LeagueEntryDTO freeQueue(String id) {
-		
+
 		BufferedReader br = null;
 		LeagueEntryDTO leagueInfo = null;
-		
-		try{            
-			String urlstr = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/"+ id + "?api_key=" + API_KEY;
+
+		try {
+			String urlstr = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + id + "?api_key="
+					+ API_KEY;
 			URL url = new URL(urlstr);
 			HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
 			urlconnection.setRequestMethod("GET");
-			br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8")); 
+			br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(), "UTF-8"));
 			String result = "";
 			String line;
-			while((line = br.readLine()) != null) { 
+			while ((line = br.readLine()) != null) {
 				result = result + line;
 			}
 			JsonParser jsonParser = new JsonParser();
 			JsonArray arr = (JsonArray) jsonParser.parse(result);
-			JsonObject k =  (JsonObject) arr.get(1);
+			JsonObject k = (JsonObject) arr.get(1);
 			int wins = k.get("wins").getAsInt();
 			int losses = k.get("losses").getAsInt();
 			String rank = k.get("rank").getAsString();
 			String tier = k.get("tier").getAsString();
 			int leaguePoints = k.get("leaguePoints").getAsInt();
-			
-			int odds = Math.round(((float) wins / (wins + losses))*100);
-			
-			leagueInfo = new LeagueEntryDTO(wins, losses, rank,tier, leaguePoints, odds);
-			
-		}catch(Exception e){
+
+			int odds = Math.round(((float) wins / (wins + losses)) * 100);
+
+			leagueInfo = new LeagueEntryDTO(wins, losses, rank, tier, leaguePoints, odds);
+
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		return leagueInfo;
 	}
-	
-	
+
+	/*
 	public List<String> matchList(String puuid) {
-		
+
 		BufferedReader br = null;
 		List<String> matchList = null;
-		
-		try{            
-			String urlstr = "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=20&api_key=" + API_KEY;
+
+		try {
+			String urlstr = "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid
+					+ "/ids?start=0&count=20&api_key=" + API_KEY;
 			URL url = new URL(urlstr);
 			HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
 			urlconnection.setRequestMethod("GET");
-			br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8")); 
+			br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(), "UTF-8"));
 			String result = "";
 			String line;
-			while((line = br.readLine()) != null) { 
+			while ((line = br.readLine()) != null) {
 				result = result + line;
 			}
 			String[] arr = new String[20];
-			
+
 			int a = 2;
 			int b = 15;
-			for(int i = 0; i < 20; i++) {
-				
+			for (int i = 0; i < 20; i++) {
+
 				arr[i] = result.substring(a, b);
 				a += 16;
 				b += 16;
 			}
-			
+
 			matchList = Arrays.asList(arr);
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		return matchList;
 	}
-
-
-
-	/*
-	 * public ParticipantDto matchGame(String matchList) {
-	 * 
-	 * BufferedReader br = null; ParticipantDto gameInfo = null;
-	 * 
-	 * try{ String urlstr = "https://asia.api.riotgames.com/lol/match/v5/matches/" +
-	 * matchList + "?api_key=" + API_KEY; URL url = new URL(urlstr);
-	 * HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
-	 * urlconnection.setRequestMethod("GET"); br = new BufferedReader(new
-	 * InputStreamReader(urlconnection.getInputStream(),"UTF-8")); String result =
-	 * ""; String line; while((line = br.readLine()) != null) { result = result +
-	 * line; } System.out.println(result);
-	 * 
-	 * JsonParser jsonParser = new JsonParser(); JsonArray arr = (JsonArray)
-	 * jsonParser.parse(result); JsonObject k = (JsonObject) arr.get(1);
-	 * 
-	 * String championName = k.get("championName").getAsString(); int champLevel =
-	 * k.get("participants.champLevel").getAsInt(); int kills =
-	 * k.get("kills").getAsInt(); int deaths = k.get("deaths").getAsInt(); int
-	 * assists = k.get("assists").getAsInt(); int item0 = k.get("item0").getAsInt();
-	 * int item1 = k.get("item1").getAsInt(); int item2 = k.get("item2").getAsInt();
-	 * int item3 = k.get("item3").getAsInt(); int item4 = k.get("item4").getAsInt();
-	 * int item5 = k.get("item5").getAsInt(); int item6 = k.get("item6").getAsInt();
-	 * boolean win = k.get("win").getAsBoolean();
-	 * 
-	 * gameInfo = new ParticipantDto(championName, champLevel, kills, deaths,
-	 * assists, item0, item1, item2, item3, item4, item5, item6, win);
-	 * 
-	 * }catch(Exception e){ System.out.println(e.getMessage()); }
-	 * 
-	 * return gameInfo; }
-	 */
+	*/
 }
